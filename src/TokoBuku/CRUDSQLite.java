bf7db066086585c3c988a7b4bc3f9f1f89794d23
ajinurofficial <ajinur.rohman@yahoo.co.id>
2017-01-02 18:30:25 +0700

    Update CRUDSQLite.java

--- src/TokoBuku/CRUDSQLite.java
+++ src/TokoBuku/CRUDSQLite.java
@@ -16,40 +16,102 @@ package TokoBuku;
  * @author Aji N. Rohman
  */
 public class CRUDSQLite implements CRUDInterface{
-
+    private Connection conn;
+    
     @Override
     public void bukaKoneksi() throws ClassNotFoundException, SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Class.forName ("org.sqlite.JDBC");
+       conn = DriverManager.getConnection("jdbc:sqlite:tokobuku.db");
     }
 
     @Override
     public void tutupKoneksi() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        conn.close();
     }
 
     @Override
     public ArrayList<TokoBuku> read() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("select * from buku");
+        ArrayList<TokoBuku> listTokoBuku = new ArrayList();
+        while (rs.next()){
+            TokoBuku ba = new TokoBuku();
+            ba.setId(rs.getInt(1));
+            ba.setJudul(rs.getString(2));
+            ba.setKategori(rs.getString(3));
+            ba.setPengarang(rs.getString(4));
+            ba.setPenerbit(rs.getString(5));
+            ba.setTahun(rs.getString(6));
+            ba.setIsbn(rs.getString(7));
+            ba.setHarga(rs.getString(8));
+            ba.setJml_hal(rs.getString(9));
+            ba.setTerbitan(rs.getString(10));            
+        }
+        return listTokoBuku;
     }
 
     @Override
     public TokoBuku readById(int id) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "select * from buku where id = ?";
+        PreparedStatement ps = conn.prepareStatement(query);  
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        TokoBuku ba = new TokoBuku();
+        while (rs.next()) {            
+            ba.setId(rs.getInt(1));
+            ba.setJudul(rs.getString(2));
+            ba.setKategori(rs.getString(3));
+            ba.setPengarang(rs.getString(4));
+            ba.setPenerbit(rs.getString(5));
+            ba.setTahun(rs.getString(6));
+            ba.setIsbn(rs.getString(7));
+            ba.setHarga(rs.getString(8));
+            ba.setJml_hal(rs.getString(9));
+            ba.setTerbitan(rs.getString(10));
+        }
+        return ba;
     }
 
     @Override
     public void create(TokoBuku ba) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "insert into buku(judul,kategori,pengarang,penerbit,tahun,isbn,harga,jml_hal,terbitan) values(?,?,?,?,?,?,?,?,?)";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, ba.getJudul());
+        ps.setString(2, ba.getKategori());
+        ps.setString(3, ba.getPengarang());
+        ps.setString(4, ba.getPenerbit());
+        ps.setString(5, ba.getTahun());
+        ps.setString(6, ba.getIsbn());
+        ps.setString(7, ba.getHarga());
+        ps.setString(8, ba.getJml_hal());
+        ps.setString(9, ba.getTerbitan());
+        ps.execute();
     }
 
     @Override
     public void delete(TokoBuku ba) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "delete from buku where id=?";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setInt(1, ba.getId());
+        ps.execute();
     }
 
     @Override
     public void update(TokoBuku ba) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "update buku set judul=?, kategori=?,pengarang=?,penerbit=?,tahun=?,isbn=?,harga=?,jml_hal=?,terbitan=? where id=?";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, ba.getJudul());
+        ps.setString(2, ba.getKategori());
+        ps.setString(3, ba.getPengarang());
+        ps.setString(4, ba.getPenerbit());
+        ps.setString(5, ba.getTahun());
+        ps.setString(6, ba.getIsbn());
+        ps.setString(7, ba.getHarga());
+        ps.setString(8, ba.getJml_hal());
+        ps.setString(9, ba.getTerbitan());
+        ps.setInt(10, ba.getId());
+        ps.execute();
     }
+
     
 }
